8fcd762da870da8d3226e8b5e3e697a9986c83aa
aphyrince <dkswjdgh01@naver.com>
2026-02-12 12:39:53 +0900

    setting 페이지 구색 맞추기

--- front-vite/src/components/Setting/SettingPage.tsx
+++ front-vite/src/components/Setting/SettingPage.tsx
@@ -1,5 +1,10 @@
 const SettingPage = () => {
-    return <div className="w-full h-200">setting page</div>;
+    return (
+        <div className="flex flex-col gap-10 w-full h-200 p-4 text-white/90">
+            <div className="w-full p-4 bg-amber-400">Mode</div>
+            <div className="w-full p-4 bg-amber-400">...</div>
+        </div>
+    );
 };
 
 export default SettingPage;
