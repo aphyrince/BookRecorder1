19ab8bdadfa04700d7690f50b4f436f40fe151eb
aphyrince <dkswjdgh01@naver.com>
2026-02-24 12:19:05 +0900

    대쉬보드에 들어갈 차트블록

--- front-vite/src/components/Dashboard/DashboardPage.tsx
+++ front-vite/src/components/Dashboard/DashboardPage.tsx
@@ -1,11 +1,9 @@
-import { useRecordStore } from "../../zustand/useRecordStore";
+import ChartBlock from "./ChartBlock";
 
 const DashboardPage = () => {
-    const { records } = useRecordStore();
-
     return (
         <div className="grid grid-cols-2 grid-rows-2 gap-4 w-full h-200 p-4 text-white/90">
-            <div className="flex p-2 bg-amber-400 rounded-2xl">block1</div>
+            <ChartBlock />
             <div className="p-2 bg-amber-400 rounded-2xl">block2</div>
             <div className="p-2 bg-amber-400 rounded-2xl">block3</div>
             <div className="p-2 bg-amber-400 rounded-2xl">block4</div>
